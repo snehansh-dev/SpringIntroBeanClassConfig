fc17332d0cafb0579223f167e6eab91b80c58cb9
Shekhar <dev.snehansh@gmail.com>
2023-01-14 15:41:07 +0530

    Bean Class Config program Basics Updated

--- src/main/java/com/snehansh/Main.java
+++ src/main/java/com/snehansh/Main.java
@@ -7,7 +7,12 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
         User user = context.getBean(User.class);
+        user.setQualification("Graduate");
         user.createUserMessage();
+        System.out.println(user.toString());
+        User user1 = context.getBean(User.class);
+        user1.createUserMessage();
+        System.out.println(user1.toString());
         Staff staff = context.getBean(Staff.class);
         staff.createUserMessage();
     }
--- src/main/java/com/snehansh/Staff.java
+++ src/main/java/com/snehansh/Staff.java
@@ -3,7 +3,6 @@ package com.snehansh;
 import java.sql.SQLOutput;
 
 public class Staff implements UserInterface{
-
     @Override
     public void createUserMessage() {
         System.out.println("Welcome to the Staff ");
